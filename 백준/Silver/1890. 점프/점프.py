783e904d4cbb74b1f7ccc705a03848fe425c6caa
YunTaeHyeon <67150311+YunTaeHyeon@users.noreply.github.com>
2024-08-06 23:57:56 +0900

    [Silver I] Title: 점프, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+board = []
+
+for _ in range(N):
+    board.append(list(map(int, input().split())))
+
+dp = [[0] * N for _ in range(N)]
+dp[0][0] = 1
+
+for i in range(N):
+    for j in range(N):
+        if dp[i][j] != 0 and board[i][j] != 0:
+            if i + board[i][j] < N:
+                dp[i + board[i][j]][j] = dp[i + board[i][j]][j] + dp[i][j]
+            if j + board[i][j] < N:
+                dp[i][j + board[i][j]] = dp[i][j+ board[i][j]] + dp[i][j]
+
+print(dp[N-1][N-1])
